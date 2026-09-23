9b33616cf07d127e9e9caf5a041440df84f0b743
aliihaydargenc-dotcom <aliihaydargenc@gmail.com>
2026-09-23 07:30:03 +0300

    feat: show nearby place names directly on map

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "yakinimda-shell-v16";
+const CACHE_NAME = "yakinimda-shell-v17";
 const RUNTIME_CACHE = "yakinimda-runtime-v1";
-const APP_SHELL = ["./", "./index.html", "./styles.css?v=2.2.2", "./app.js?v=2.2.2", "./manifest.webmanifest", "./icon.svg"];
+const APP_SHELL = ["./", "./index.html", "./styles.css?v=2.2.3", "./app.js?v=2.2.3", "./manifest.webmanifest", "./icon.svg"];
 const LEAFLET_ORIGIN = "https://unpkg.com";
 
 self.addEventListener("install", (event) => {
