2ded108b03244208ab8c30f26362e980054827c9
aliihaydargenc-dotcom <aliihaydargenc@gmail.com>
2026-09-24 07:32:00 +0300

    chore: align sw.js with mobile v3.6

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "yakinimda-shell-v47";
+const CACHE_NAME = "yakinimda-shell-v49";
 const RUNTIME_CACHE = "yakinimda-runtime-v1";
-const APP_SHELL = ["./", "./index.html", "./styles.css?v=3.5.1", "./app.js?v=3.5.0", "./manifest.webmanifest", "./icon.svg"];
+const APP_SHELL = ["./", "./index.html", "./styles.css?v=3.6.0", "./app.js?v=3.6.0", "./manifest.webmanifest", "./icon.svg"];
 const LEAFLET_ORIGIN = "https://unpkg.com";
 
 self.addEventListener("install", (event) => {
